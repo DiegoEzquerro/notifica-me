7aadf082893121530b936c48ef916580b03f7672
Diego Ezquerro <dezquerro@fcirce.es>
2022-06-27 15:48:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import firebase from './firebase';
 
 function App() {
+  const [token, setToken] = useState(''); 
+
+  useEffect(()=>{
+    const msg=firebase.messaging();
+    msg.requestPermission().then(()=>{
+      return msg.getToken();
+    }).then((data)=>{
+      console.warn("token",data);
+      setToken(data);
+    })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notifica-me</h1>
+      <h3>mi token es</h3>
+      <h3>{token}</h3>
     </div>
   );
 }
